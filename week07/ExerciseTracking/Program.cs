d4add7d98c96a3d4e89442e0c69c6d02d3097ce5
Vlad <vlad.didkovskiy.it@gmail.com>
2025-02-20 20:36:25 +0200

    Exercise Tracking

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,23 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>();
+
+        Running run = new Running(new DateTime(2022, 11, 3), 30, 3.0); // 30 min, 3 miles
+        Cycling bike = new Cycling(new DateTime(2022, 11, 3), 30, 12.0); // 30 min, 12 mph
+        Swimming swim = new Swimming(new DateTime(2022, 11, 3), 30, 20); // 30 min, 20 laps
+
+        activities.Add(run);
+        activities.Add(bike);
+        activities.Add(swim);
+
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
